e2018e4680b87a18731794ea2838d60d7ebb8873
Mishel Ghukasyan <55681212+Mish2j@users.noreply.github.com>
2025-04-10 18:33:28 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		 String url = "jdbc:mysql://localhost:3306/food_delivery";  // Replace with your DB name
+		 String url = "jdbc:mysql://localhost:3306/food_delivery";
 	     String user = "root"; // MySQL username
 	     String password = "root"; // MySQL password
 		
